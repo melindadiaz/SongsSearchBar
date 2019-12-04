05cfb619bd57fea6004a472e28ffd75dea44280c
melinda <melindad_82@msn.com>
2019-12-03 19:31:51 -0500

    Completed search bar function

--- SongsTableViewSearchBar/ViewController.swift
+++ SongsTableViewSearchBar/ViewController.swift
@@ -9,47 +9,81 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var songSearch: UISearchBar!
-    
     @IBOutlet weak var songTableView: UITableView!
-    
+
+    //use to represent what the user is searching
+    var userQuery = "" {
+        //when the property observer that is attached to changes then DidSet gets triggered
+        didSet { //trailing closure syntax
+            songs = Song.loveSongs.filter{$0.name.lowercased().contains(userQuery.lowercased())}
+        }
+    }
     //This just grabs the data within the class, because of the static func
-    var songs = Song.loveSongs
+    var songs = [Song]() {
+        didSet {
+            songTableView.reloadData()
+        }
+    }
+    var isSearchBarEmpty: Bool {
+        return songSearch.text?.isEmpty ?? true
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUp()
         songTableView.delegate = self
         songTableView.dataSource = self
+        songSearch.delegate = self
+        definesPresentationContext = true
     }
     
-    
+    func setUp() {
+        songs = Song.loveSongs
+    }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        songs.count
+        return songs.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "songCell") else { return UITableViewCell()}
+        let cell = tableView.dequeueReusableCell(withIdentifier: "songCell", for: indexPath)
         
-         let info = songs[indexPath.row]
-        cell.textLabel!.text = info.artist
-        cell.detailTextLabel!.text = info.name
+        cell.textLabel!.text = songs[indexPath.row].name
+        cell.detailTextLabel!.text = songs[indexPath.row].artist
+        cell.textLabel?.numberOfLines = 0 // this creates room for the text inside the label/tablecell without it cutting off
         return cell
     }
     
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let detailViewController = segue.destination as? DetailViewController,
-        let indexPath = songTableView.indexPathForSelectedRow else {
-            return
+            let indexPath = songTableView.indexPathForSelectedRow else {
+                return
         }
         detailViewController.navigationItem.title = songs[indexPath.row].artist
         detailViewController.viewTwoSong = [songs[indexPath.row]]
-        
-        }
     }
-    
-    
+}
 
+
+extension ViewController: UISearchBarDelegate {
+    //has a bunch of optional methods in the palm of your hands
+    
+    //When the user presses the magnify glass or enter
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let updatedUserQuery = searchBar.text else {
+            return
+        }
+        userQuery = updatedUserQuery //Set the value of means equal
+        print(userQuery)
+    }
+    //this is optional chaining
+    //        guard !searchBar.text?.isEmpty else {
+    //            songs = Song.loveSongs
+}
